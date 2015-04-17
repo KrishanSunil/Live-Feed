9a104ddd34d0c4a3637f1803129e9e860662a7a5
KrishanSunil <KrishanS@Mac02s-Mac-Pro.local>
2015-04-17 17:14:43 +0800

    Initial Commit
    Initial Commit

---
+++ Live and Feed/Live and Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Live and Feed
+//
+//  Created by Krishantha Sunil on 17/4/15.
+//  Copyright (c) 2015 FIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Live and Feed/Live and Feed/ViewController/LiveViewController/LiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LiveViewController.swift
+//  Live and Feed
+//
+//  Created by Krishantha Sunil on 17/4/15.
+//  Copyright (c) 2015 FIC. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
